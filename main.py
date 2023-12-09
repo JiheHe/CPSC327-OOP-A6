4d825061271b1ac91a787250178e030937e35ec0
JiheHe <nickhe2003@gmail.com>
2023-12-09 15:05:17 -0500

    fdfsaa

--- main.py
+++ main.py
@@ -107,9 +107,3 @@ class GameCLI:
 
 if __name__=='__main__':
   GameCLI().run()
-  # test = Game("human", "heuristic", None, None)
-  # # test2 = Game(True, True, False, False)
-  # # print(test2)
-  # print(test)
-  # test._players[0]._workers['A'].move('n')
-  # print(test)
